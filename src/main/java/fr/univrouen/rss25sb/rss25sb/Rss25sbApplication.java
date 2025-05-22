cef10c2c3657e7ebc88a24965e45772da4bdf3d9
Mzah Sarra <mzahsarra@gmail.com>
2025-05-22 16:41:02 +0200

    first commit

---
+++ src/main/java/fr/univrouen/rss25sb/rss25sb/Rss25sbApplication.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss25sb.rss25sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rss25sbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rss25sbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univrouen/rss25sb/rss25sb/Rss25sbApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.rss25sb.rss25sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rss25sbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
